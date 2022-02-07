fbcd0cb9ae2b048d70aebeb676528b5219edf626
Justus Bayat <61127228+p47even@users.noreply.github.com>
2022-02-06 17:05:57 -0800

    Added schemas

---
+++ canSchema.sql
@@ -0,0 +1,45 @@
+PRAGMA foreign_keys = ON;
+
+--delete tables if already exist
+drop table if exists CannedGoods;
+drop table if exists Jars;
+drop table if exists Batch;
+drop table if exists Shelves;
+drop table if exists Storage;
+
+--schema
+create table Storage(
+    locationID integer primary key,
+    locationName text
+);
+
+create table Shelves(
+    shelfID integer,
+    locationID integer,
+    shelfName text,
+    primary key (shelfID, locationID),
+    foreign key (locationID) references Storage (locationID)
+);
+
+create table Batch(
+    batchID integer primary key,
+    product text,
+    datePlaced text check (datePlaced glob '[0-9][0-9]/[0-9][0-9]/[0-9][0-9]'),
+    shelfID integer, 
+    quantity integer check (quantity >= 0),
+    foreign key (shelfID) references Shelves(shelfID)
+);
+
+create table Jars(
+    jarID integer primary key,
+    size text,
+    mouth text check (mouth = 'regular' or mouth = 'wide')
+);
+
+create table CannedGoods(
+    jarID integer,
+    batchID integer,
+    primary key (jarID, batchID),
+    foreign key (jarID) references Jars(jarID),
+    foreign key (batchID) references Batch(batchID)
+);
---
+++ generalSchema.sql
@@ -0,0 +1,51 @@
+PRAGMA foreign_keys = ON;
+
+--delete tables if already exist
+drop table if exists Stock;
+drop table if exists Shelf;
+drop table if exists Expiration;
+drop table if exists WishList;
+drop table if exists Location;
+drop table if exists Product;
+
+--schema
+create table Product(
+    productID integer primary key,
+    productName text,
+    notes text
+);
+
+create table Location(
+    locationID integer primary key,
+    locationName text
+);
+
+create table WishList(
+    productID integer primary key,
+    foreign key (productID) references Product(productID) 
+);
+
+create table Expiration(
+    productID integer primary key,
+    expirationDate text check (expirationDate glob '[0-9][0-9]/[0-9][0-9]/[0-9][0-9]'),
+    foreign key (productID) references Product (productID) 
+);
+
+create table Shelf(
+    shelfID integer,
+    locationID integer,
+    shelfName text,
+    primary key (shelfID, locationID),
+    foreign key (locationID) references Location (locationID)
+);
+
+create table Stock(
+    productID integer,
+    shelfID integer,
+    locationID integer,
+    datePurchased text check (datePurchased glob '[0-9][0-9]/[0-9][0-9]/[0-9][0-9]'),
+    quantity integer check (quantity >= 0),
+    primary key(productID,shelfID,locationID),
+    foreign key (productID) references Product(productID),
+    foreign key (shelfID, locationID) references Shelf(shelfID,locationID)
+);
