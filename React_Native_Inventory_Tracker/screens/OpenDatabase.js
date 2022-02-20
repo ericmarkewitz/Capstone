aadf2c775e40a4a2a2323037188b4e2ddc680492
ericmarkewitz <61162496+ericmarkewitz@users.noreply.github.com>
2022-02-20 13:29:37 -0800

    Added opening the database and running exection statements to the opendatabase screen

--- React_Native_Inventory_Tracker/screens/OpenDatabase.js
+++ React_Native_Inventory_Tracker/screens/OpenDatabase.js
@@ -16,6 +16,91 @@ function OpenDatabase(props){
           console.log("Database OPENED");
         },
 
-        const db = SQLite.openDatabase("../../db", "1.0", "Can Database", 200000, openCB, errorCB);
+        const db = SQLite.openDatabase({
+            name : 'canning',
+            location: './sql/canning.db'
+            },
+            ()=>{},
+            error=>{console.log("error")}
+        );
+
+        const createTables = () => {
+            db.transaction((tx) =>{
+                tx.executeSql(
+                    "PRAGMA foreign_keys = ON;"
+                )
+                tx.executeSql(
+                    "CREATE TABLE IF NOT EXISTS Storage("
+                    + "locationID integer primary key, "
+                    + "locationName text"
+                    + ");"
+                )
+                tx.executeSql(
+                    "CREATE TABLE IF NOT EXISTS Shelves("
+                    + "shelfID integer,"
+                    + "locationID integer,"
+                    + "shelfName text,"
+                    + "primary key (shelfID, locationID),"
+                    + "foreign key (locationID) references Storage (locationID)"
+                    + ");"
+                )
+                tx.executeSql(
+                    "CREATE TABLE IF NOT EXISTS Batch("
+                    + "batchID integer primary key,"
+                    + "product text,"
+                    + "datePlaced text check (datePlaced glob '[0-9][0-9]/[0-9][0-9]/[0-9][0-9]'),"
+                    + "shelfID integer,"
+                    + "quantity integer check (quantity >= 0),"
+                    + "foreign key (shelfID) references Shelves(shelfID)"
+                    + ");"
+                )
+                tx.executeSql(
+                    "CREATE TABLE IF NOT EXISTS Jars("
+                    + "jarID integer primary key,"
+                    + "size text,"
+                    + "mouth text check (mouth = 'regular' or mouth = 'wide')"
+                    + ");"
+                )
+                tx.executeSql(
+                    "CREATE TABLE IF NOT EXISTS CannedGoods("
+                    + "jarID integer,"
+                    + "batchID integer,"
+                    + "primary key (jarID, batchID),"
+                    + "foreign key (jarID) references Jars(jarID),"
+                    + "foreign key (batchID) references Batch(batchID)"
+                    + ");"
+                )
+
+                tx.executeSql(
+                    "INSERT INTO Storage (locationID, locationName) VALUES (?,?);",
+                    [1, "FirstLocation"]
+                )
+                tx.executeSql(
+                    "INSERT INTO Shelves (shelfID, locationID, shelfName) VALUES (?,?,?);",
+                    [1, 1, "FirstShelf"]
+                )
+                tx.executeSql(
+                    "INSERT INTO Batch (batchID, product, datePlaced, shelfID, quantity) VALUES (?,?,?,?,?);",
+                    [1, "FirstProduct", "00/00/00", 1, 1]
+                )
+                tx.executeSql(
+                    "INSERT INTO Jars (jarID, size, mouth) VALUES (?,?,?);",
+                    [1, "big", "regular"]
+                )
+                tx.executeSql(
+                    "INSERT INTO CannedGoods (jarID, batchID) VALUES (?,?);",
+                    [1, 1]
+                )
+
+
+            })
+        }
+
+
+
+
+
+
+
     );
 }
